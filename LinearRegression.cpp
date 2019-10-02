db0614bae77f947edb61c720111068bee479aa34
张峰 <yonghenghuanmie@hotmail.com>
2019-10-03 03:23:14 +0800

    optimize

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -7,10 +7,8 @@
 //θ₁=10,θ₂=150,X₂=1
 std::tuple<Eigen::MatrixX2d, Eigen::VectorXd> RandomGenterateTrainSet(size_t counts)
 {
-	Eigen::MatrixX2d train_input;
-	Eigen::VectorXd train_output;
-	train_input.resize(counts, Eigen::NoChange);
-	train_output.resize(counts, Eigen::NoChange);
+	Eigen::MatrixX2d train_input(counts, 2);
+	Eigen::VectorXd train_output(counts);
 	for (size_t i = 0; i < counts; i++)
 	{
 		float X₁ = std::rand() % 100 + 20, X₂ = 1;
@@ -29,7 +27,6 @@ std::tuple<Eigen::MatrixX2d, Eigen::VectorXd> RandomGenterateTrainSet(size_t cou
 Eigen::MatrixXd LossFunction(const Eigen::MatrixXd& model, const Eigen::MatrixXd& train_input, const Eigen::MatrixXd& train_output)
 {
 	auto hx_sub_jx = train_output - train_input * model;
-	//not sure
 	return hx_sub_jx.array().pow(2) / (train_input.rows() * 2);
 }
 
@@ -50,9 +47,10 @@ void GradientDescent(Eigen::MatrixXd& model, const Eigen::MatrixXd& train_input,
 			{
 				reference, train_input.row(train_index);
 			}
+			Eigen::MatrixXd update = learning_rate.array() * (1.0 / train_input.rows() * hx_sub_jx.row(train_index) * duplicate_line).transpose().array();
 			/*std::cout << duplicate_line << "\n\n";
-			std::cout << Eigen::MatrixXd(learning_rate.array() * (1.0 / train_input.rows() * hx_sub_jx.row(train_index) * duplicate_line).transpose().array()) << "\n\n";*/
-			model += Eigen::MatrixXd(learning_rate.array() * (1.0 / train_input.rows() * hx_sub_jx.row(train_index) * duplicate_line).transpose().array());
+			std::cout << update << "\n\n";*/
+			model += update;
 			std::cout << model << "\n\n";
 		}
 	}
@@ -64,9 +62,9 @@ int main()
 	/*std::cout << train_input << std::endl;
 	std::cout << train_output << std::endl;*/
 
-	Eigen::MatrixXd model = Eigen::Vector2d(1, 100);
-	Eigen::VectorXd learning_rate = Eigen::Vector2d(0.001, 0.25);
-	int batch_size = 100;
+	Eigen::MatrixXd model = Eigen::Vector2d(1, 10);
+	Eigen::Vector2d learning_rate(0.001, 0.25);
+	int batch_size = 200;
 	GradientDescent(model, train_input, train_output, learning_rate, batch_size);
 	return 0;
 }
